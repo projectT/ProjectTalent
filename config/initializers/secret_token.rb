3cb33dc80d1b149ff9fb69bf306a36571e5b89ff
unknown <King@.(none)>
2010-11-19 11:05:39 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectTalent::Application.config.secret_token = '748bd8e5832c66773f6d64788ab3790a29ebac1b214311aff64858cae48d53c39fea764e0c752d5fbe259321205b819a67dcc435c0bb9f70d92ee4d85981c09d'
